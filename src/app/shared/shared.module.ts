d2028fd868b454a8dc79040523550a4baa16fce3
e3c_100005333972_iven <e3c_100005333972_iven@git.cloud.tencent.com>
2018-09-05 23:25:37 +0800

    init

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import {MatButtonModule, MatCheckboxModule,MatToolbarModule,MatIconModule,MatCardModule,MatInputModule,MatGridListModule,MatDialogModule} from '@angular/material';
+
 @NgModule({
   imports: [
     CommonModule, 
